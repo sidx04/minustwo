474cd3da41449f752773a9fda5d3cbe27528d6ff
sidx04 <siddpal04@gmail.com>
2025-04-11 01:21:48 +0530

    feat: push (remaining) and dup

--- src/opcodes/stack_ops.rs
+++ src/opcodes/stack_ops.rs
@@ -2,25 +2,45 @@ use super::Opcode;
 use crate::{errors::stack::StackError, machine::Machine};
 use primitive_types::U256;
 
-pub fn execute_stack(op: Opcode, machine: &mut Machine) -> Result<(), StackError> {
-    let _ = match op {
-        Opcode::PUSH1 => {
-            let pc = machine.pc;
+pub fn execute_stack(op: Opcode, _offset: usize, machine: &mut Machine) -> Result<(), StackError> {
+    let pc = machine.pc;
 
-            if pc + 1 >= machine.code.len() {
-                return Err(StackError::InvalidItem);
-            }
+    if pc + 1 >= machine.code.len() {
+        return Err(StackError::InvalidItem);
+    }
 
-            let value = machine.code[pc + 1];
+    let value = machine.code[pc + 1];
 
-            machine.stack.push(U256::from(value))?;
+    machine.stack.push(U256::from(value))?;
 
-            println!("{}", machine.stack);
+    println!("{}", machine.stack);
 
-            // increment PC to skip the operand (1 byte)
-            machine.pc += 2;
-        }
-        _ => todo!(),
-    };
+    // increment PC to skip the operand (1 byte)
+    machine.pc += 2;
+    Ok(())
+}
+
+pub fn execute_stack_duplicate(
+    _op: Opcode,
+    index: usize,
+    machine: &mut Machine,
+) -> Result<(), StackError> {
+    let stack = &mut machine.stack;
+
+    let contents = stack.get_contents();
+
+    let dup_index = contents.iter().enumerate().len() - index;
+    println!("{}", dup_index);
+
+    if dup_index > stack.get_contents().len() {
+        return Err(StackError::InvalidItem);
+    }
+
+    let value = contents.get(dup_index).ok_or(StackError::EmptyStack)?;
+
+    stack.push(*value)?;
+
+    machine.pc += index;
+
     Ok(())
 }
