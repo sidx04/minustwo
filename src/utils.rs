c34b719bb0a0a66736ef8d7fd4a196b33988c662
sidx04 <siddpal04@gmail.com>
2025-04-25 20:31:47 +0530

    chore: change setup to actual account hash

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
-use std::rc::Rc;
+use primitive_types::{H160, U256};
+use std::{rc::Rc, str::FromStr};
 
 use crate::{
     execution::{ExecutionContext, gas::GasMeter},
@@ -11,18 +12,17 @@ pub fn setup(
     gas_limit: u64,
     memory_limit: usize,
 ) -> ExecutionContext<'static> {
-    use primitive_types::{H160, U256};
     use std::collections::HashMap;
 
     let mut state = AccountState {
         accounts: HashMap::new(),
     };
 
-    let caller = H160::from_low_u64_be(0xabc);
-    let callee = H160::from_low_u64_be(0xdef);
+    let caller = H160::from_str("0xbe862ad9abfe6f22bcb087716c7d89a26051f74c").unwrap();
+    let callee = H160::from_str("0x9bbfed6889322e016e0a02ee459d306fc19545d8").unwrap();
 
     state.create_account(caller, U256::from(1000), caller_code);
-    state.create_account(callee, U256::from(1), callee_code.clone());
+    state.create_account(callee, U256::from(100), callee_code.clone());
 
     let code = Rc::new(callee_code);
     let machine = Box::leak(Box::new(Machine::new(code, Rc::new(vec![]), memory_limit))); // Leak to extend lifetime
