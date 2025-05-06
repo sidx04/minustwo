9b466f4c2c3b68eec211f92c1384e0c3d6cae551
sidx04 <siddpal04@gmail.com>
2025-05-07 01:55:39 +0530

    refactor: hashing seperated

--- src/opcodes/hash_ops.rs
+++ src/opcodes/hash_ops.rs
@@ -1,29 +1,21 @@
-use alloy_primitives::Keccak256;
 use primitive_types::U256;
 
-use crate::{errors::Error, execution::ExecutionContext};
+use crate::{errors::Error, execution::ExecutionContext, utils::hash_keccak};
 
 use super::Opcode;
 
 pub fn execute_keccak256(_op: Opcode, ctx: &mut ExecutionContext) -> Result<(), Error> {
     let stack = &mut ctx.machine.stack;
     let memory = &mut ctx.machine.memory;
-    let pc = &mut ctx.machine.pc;
-    let mut hasher = Keccak256::new();
 
     let offset = stack.pop()?;
     let size = stack.pop()?;
 
     let mut data = memory.access(offset.as_usize(), size.as_usize())?;
-
-    hasher.update(&mut data);
-
-    let mut output = [0u8; 32];
-    hasher.finalize_into_array(&mut output);
-
+    let output = hash_keccak(&mut data)?;
     stack.push(U256::from_big_endian(&output))?;
 
-    *pc += 1;
+    ctx.machine.pc += 1;
 
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,12 @@
+use alloy_primitives::Keccak256;
 use primitive_types::{H160, U256};
 use std::{rc::Rc, str::FromStr};
 
 use crate::{
+    errors::Error,
     execution::{ExecutionContext, gas::GasMeter},
-    machine::{Machine, state::AccountState},
+    machine::{Machine, state::State},
+    storage::kv::KeyValueStore,
 };
 
 pub fn setup(
@@ -14,8 +17,9 @@ pub fn setup(
 ) -> ExecutionContext<'static> {
     use std::collections::HashMap;
 
-    let mut state = AccountState {
+    let mut state = State {
         accounts: HashMap::new(),
+        storage: KeyValueStore::new(), // change later to account global state
     };
 
     let caller = H160::from_str("0xbe862ad9abfe6f22bcb087716c7d89a26051f74c").unwrap();
@@ -33,7 +37,17 @@ pub fn setup(
         caller,
         callee,
         value: U256::zero(),
-        input_data: vec![],
+        calldata: vec![0xFF, 0xEE, 0xAA, 0xBB, 0xCC, 0xDD, 0x12, 0x34, 0x56],
         gas_meter: GasMeter::new(gas_limit),
     }
+}
+
+pub fn hash_keccak(bytes: &mut Vec<u8>) -> Result<[u8; 32], Error> {
+    let mut hasher = Keccak256::new();
+    let mut output = [0u8; 32];
+
+    hasher.update(bytes);
+    hasher.finalize_into_array(&mut output);
+
+    Ok(output)
 }
